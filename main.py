57ef9131158f51f4e176abd9b4f94858c6b23099
programmist <programmist.030@gmail.com>
2023-11-19 19:09:05 +0400

    Some changes in variable names

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ ic(ad_accs_data)
 
 # Getting client ids of VK ad accounts
 client_ids = pd.Series()
-for index, account in ad_accs_data.iterrows():
+for account_id, account in ad_accs_data.iterrows():
     if account.account_type == "general":
-        client_ids[index] = None
+        client_ids[account_id] = None
         continue
     r = requests.post(f"{API_ADDRESS}ads.getClients", params={
         "access_token": token,
         "v": "5.131",
-        "account_id": account.name}).json()
+        "account_id": account_id}).json()
     data = r.get("response") if "response" in r else r.get("error")
-    client_ids[index] = [i.get("id") for i in data]
+    client_ids[account_id] = [i.get("id") for i in data]
     time.sleep(0.5)
 ad_accs_data["client_ids"] = client_ids
 ic(ad_accs_data)
