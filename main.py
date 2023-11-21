b342f7cd00d630afbb34d0baded3a9b22e1be41b
programmist <programmist.030@gmail.com>
2023-11-21 20:39:15 +0400

    Getting client ids of VK ad accounts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from icecream import ic
 import requests
 import pandas as pd
 import time
+import sys
 
 API_ADDRESS = "https://api.vk.com/method/"
 OAUTH_ENDPOINT = "https://oauth.vk.com/"
@@ -17,22 +18,28 @@ r = requests.post(f"{API_ADDRESS}ads.getAccounts",
 data = r.get("response") if "response" in r else r.get("error")
 ic(data)
 ad_accs_data = pd.DataFrame(
-    data=data, columns=["account_id", "account_type", "account_name"])
-ad_accs_data.set_index("account_id", inplace=True, drop=True)
+    data, columns=["account_id", "account_type", "account_name"])
+ad_accs_data.rename(
+    columns={"account_id": "id", "account_type": "type", "account_name": "name"}, inplace=True)
 ic(ad_accs_data)
 
 # Getting client ids of VK ad accounts
-client_ids = pd.Series()
-for account_id, account in ad_accs_data.iterrows():
-    if account.account_type == "general":
-        client_ids[account_id] = None
+client_ids = pd.DataFrame(
+    columns=["client_id", "account_id"])
+for index, account in ad_accs_data.iterrows():
+    account_id = account.id
+    if account.type == "general":
         continue
     r = requests.post(f"{API_ADDRESS}ads.getClients", params={
         "access_token": token,
         "v": "5.131",
         "account_id": account_id}).json()
-    data = r.get("response") if "response" in r else r.get("error")
-    client_ids[account_id] = [i.get("id") for i in data]
+    data = r.get("response") if "response" in r else None
+    ic(data)
+    clients = pd.DataFrame(data, columns=["id", "name"])
+    ic(clients)
+    clients.rename(columns={"id": "client_id"}, inplace=True)
+    ic(clients)
+    client_ids = pd.concat([client_ids, clients])
     time.sleep(0.5)
-ad_accs_data["client_ids"] = client_ids
-ic(ad_accs_data)
+ic(client_ids)
