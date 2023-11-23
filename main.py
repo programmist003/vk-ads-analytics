c9914ec83670462278b0cbab0723e59ae6af6b77
programmist <programmist.030@gmail.com>
2023-11-23 20:32:11 +0400

    Getting ads

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import requests
 import pandas as pd
 import time
 import sys
+from collections import OrderedDict
 
 API_ADDRESS = "https://api.vk.com/method/"
 OAUTH_ENDPOINT = "https://oauth.vk.com/"
+VERSION = "5.131"
 
 config = toml.load("config.toml")
 token = config.get("token")
@@ -37,7 +39,7 @@ for index, account in ad_accs_data.iterrows():
         continue
     r = requests.post(f"{API_ADDRESS}ads.getClients", params={
         "access_token": token,
-        "v": "5.131",
+        "v": VERSION,
         "account_id": account.id}).json()
     data = r.get("response") if "response" in r else None
     clients = pd.DataFrame(data, columns=["id", "name"])
@@ -45,3 +47,22 @@ for index, account in ad_accs_data.iterrows():
     client_ids = pd.concat([client_ids, clients])
     time.sleep(0.5)
 ic(client_ids)
+
+# Getting ads
+ad_ids = pd.DataFrame(columns=["campaign_id", "id", "client_id"])
+for index, client in client_ids.iterrows():
+    client_id = {client.id: "client_id", client.owner_id: "account_id"}
+    client_id = {v: k for k, v in client_id.items()}
+    params = {
+        'access_token': token,
+        'v': VERSION,
+    }
+    params.update(client_id)
+    r = requests.get('https://api.vk.com/method/ads.getAds',
+                     params=params).json()
+    data = r.get("response") if "response" in r else None
+    ads = pd.DataFrame(data, columns=["campaign_id", "id"])
+    ads["client_id"] = client.id
+    ad_ids = pd.concat([ad_ids, ads])
+    time.sleep(0.5)
+ic(ad_ids)
