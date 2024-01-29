891e980e416ca1ad0558a9b57f7d47719add3fee
programmist <programmist.030@gmail.com>
2024-01-30 02:22:31 +0400

    export.py: upload_gsheet gets small improves

--- export.py
+++ export.py
@@ -4,13 +4,15 @@ from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from app import config
-      
+
+
 def upload_gsheet(name, sheet_name, dataframe):
-   creds_file = "credentials.json"
-   scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-   creds = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
-   gc = gspread.authorize(creds)
-   sh = gc.create(name)
-   worksheet = sh.add_worksheet(sheet_name, 1000, 1000)
-   worksheet.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
-   sh.share(config.gmail, perm_type='user', role='writer')
+    creds_file = "credentials.json"
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    gc = gc = gspread.service_account(creds_file)
+    sh = gc.create(name)
+    worksheet = sh.add_worksheet(sheet_name, 1000, 1000)
+    worksheet.update([dataframe.columns.values.tolist()] +
+                     dataframe.values.tolist())
+    sh.share(config.gmail, perm_type='user', role='writer')
